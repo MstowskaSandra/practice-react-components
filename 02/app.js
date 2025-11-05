5cf3ad884fa6939b693bb7f9c2097eae6af079e7
Sandra <sandra.mstowskaa@gmail.com>
2025-11-05 13:40:00 +0100

    task02: feat: add clickHandler in counter

--- 02/app.js
+++ 02/app.js
@@ -4,12 +4,22 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class Counter extends React.Component {
-    state = {
-        amount: 0,
+    constructor(props) {
+        super(props);
+        this.state = { amount: 0 };
+        this.clickHandler = this.clickHandler.bind(this);
     }
+     clickHandler() {
+        this.setState((prevState) => ({ amount: prevState.amount + 1}));
+        console.log('Click!');
+     }
     
     render() {
-        return <button>click me ({ this.state.amount })</button>
+        return (
+            <button onClick={this.clickHandler}>
+                click me ({ this.state.amount })
+            </button>
+        );
     }
 }
 

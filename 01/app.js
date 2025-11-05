d32d1e30d98c8ce43ccf3cd04a12ed717a844c28
Sandra <sandra.mstowskaa@gmail.com>
2025-11-05 13:18:26 +0100

    task01: feat: add interval counter

--- 01/app.js
+++ 01/app.js
@@ -4,13 +4,30 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class App extends React.Component {
-    state = {
-        counter: 0,
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0};
+        console.log('constructor');
     }
+
     render() {
         console.log('render');
-
         return <h1>{ this.state.counter }</h1>
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+        this.id = setInterval(() => {
+            const {counter} = this.state;
+            this.setState({
+                counter: counter +1,
+            });
+        }, 5000);           
+    }
+    
+    componentWillUnmount() {
+        clearInterval( this.id );
+        console.log('componentWillUnmount');
     }
 }
 
